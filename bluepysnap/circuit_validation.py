252bc566125117c8e15b841d26d23f2ebf3e1b37
Mike Gevaert <mgevaert@openbraininstitute.org>
2026-07-21 14:53:09 +0200

    have validated files to test with

--- bluepysnap/circuit_validation.py
+++ bluepysnap/circuit_validation.py
@@ -521,7 +521,9 @@ def validate_edge_population(edges_file, name, nodes):
     return []
 
 
-def validate_edges_dict(edges_dict, nodes, skip_slow, ignore_datatype_errors):
+def validate_edges_dict(
+    edges_dict, nodes, skip_slow, ignore_datatype_errors, ignore_edge_properties
+):
     """Validate an item in the "edges" list.
 
     Args:
@@ -529,6 +531,7 @@ def validate_edges_dict(edges_dict, nodes, skip_slow, ignore_datatype_errors):
         nodes (list): "nodes" part of the resolved bluepysnap config
         skip_slow(bool): skip slow tests
         ignore_datatype_errors(bool): Don't check if datatypes are correct
+        ignore_edge_properties(Iterable[str]): Edge properties to ignore when validating
 
     Returns:
         list: List of errors, empty if no errors
@@ -570,13 +573,14 @@ def validate_edges_dict(edges_dict, nodes, skip_slow, ignore_datatype_errors):
     return errors
 
 
-def validate_nodes_dict(nodes_dict, components, ignore_datatype_errors):
+def validate_nodes_dict(nodes_dict, components, ignore_datatype_errors, ignore_node_properties):
     """Validate an item in the "nodes" list.
 
     Args:
         nodes_dict (dict): nodes population, represented by an item of "nodes" in ``config``
         components(dict): "components" part of the ``config``
         ignore_datatype_errors(bool): Don't check if datatypes are correct
+        ignore_node_properties(Iterable[str]): Node properties to ignore when validating
 
     Returns:
         list: List of errors, empty if no errors
@@ -598,7 +602,9 @@ def validate_nodes_dict(nodes_dict, components, ignore_datatype_errors):
     return errors
 
 
-def validate_networks(config, skip_slow, ignore_datatype_errors):
+def validate_networks(
+    config, skip_slow, ignore_datatype_errors, ignore_node_properties, ignore_edge_properties
+):
     """Validate "networks" part of the config.
 
     Acts as a starting point of validation.
@@ -612,16 +618,26 @@ def validate_networks(config, skip_slow, ignore_datatype_errors):
 
     for nodes_dict in nodes:
         if "nodes_file" in nodes_dict:
-            errors += validate_nodes_dict(nodes_dict, components, ignore_datatype_errors)
+            errors += validate_nodes_dict(
+                nodes_dict, components, ignore_datatype_errors, ignore_node_properties
+            )
     for edges_dict in config["networks"].get("edges", []):
         if "edges_file" in edges_dict:
-            errors += validate_edges_dict(edges_dict, nodes, skip_slow, ignore_datatype_errors)
+            errors += validate_edges_dict(
+                edges_dict, nodes, skip_slow, ignore_datatype_errors, ignore_edge_properties
+            )
 
     return errors
 
 
 def validate(
-    config_file, skip_slow, only_errors=False, print_errors=True, ignore_datatype_errors=False
+    config_file,
+    skip_slow,
+    only_errors=False,
+    print_errors=True,
+    ignore_datatype_errors=False,
+    ignore_node_properties=(),
+    ignore_edge_properties=(),
 ):
     """Validates Sonata circuit.
 
@@ -631,6 +647,8 @@ def validate(
         only_errors (bool): only return/print fatal errors
         print_errors (bool): print errors
         ignore_datatype_errors(bool): Don't check if datatypes are correct
+        ignore_node_properties(Iterable[str]): Node properties to ignore when validating
+        ignore_edge_properties(Iterable[str]): Edge properties to ignore when validating
 
     Returns:
         set: set of errors, empty if no errors
@@ -639,7 +657,13 @@ def validate(
     errors = schemas.validate_circuit_schema(config_file, config, ignore_datatype_errors)
 
     if "networks" in config:
-        errors += validate_networks(config, skip_slow, ignore_datatype_errors)
+        errors += validate_networks(
+            config,
+            skip_slow,
+            ignore_datatype_errors,
+            ignore_node_properties,
+            ignore_edge_properties,
+        )
 
     if _check_partial_circuit_config(config):
         message = (
@@ -648,6 +672,7 @@ def validate(
         )
         L.warning(message)
         errors.append(BluepySnapValidationError.warning(message))
+
     if only_errors:
         errors = [e for e in errors if e.level == BluepySnapValidationError.FATAL]
 
